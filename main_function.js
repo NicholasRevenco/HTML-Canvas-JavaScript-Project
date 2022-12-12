1f8a25cfdc9b1ac62946d7e47603ea24d771c487
NicholasRevenco <nicholas.revenco.25@bishops.com>
2022-12-12 11:12:20 -0800

    Draw black square in center of canvas

--- main_function.js
+++ main_function.js
@@ -15,4 +15,15 @@ function setUpContext() {
 
 setUpContext();
 
-window.requestAnimationFrame();
+function draw() {
+  window.requestAnimationFrame(draw);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.beginPath();
+    context.rect(canvas.width / 2, canvas.height / 2, 50, 50);
+    context.fillStyle = "#000000";
+    context.fillRect(canvas.width / 2, canvas.height / 2, 50, 50);
+    context.stroke();
+
+}
+
+window.requestAnimationFrame(draw);
