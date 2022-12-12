ec1ab7759ba96d91d8620213562a5fd5ad28bdea
NicholasRevenco <nicholas.revenco.25@bishops.com>
2022-12-12 11:40:55 -0800

    Move rectanlge

--- main_function.js
+++ main_function.js
@@ -12,18 +12,49 @@ function setUpContext() {
 
   return context;
 }
-
 setUpContext();
 
-function draw() {
-  window.requestAnimationFrame(draw);
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.beginPath();
-    context.rect(canvas.width / 2, canvas.height / 2, 50, 50);
-    context.fillStyle = "#000000";
-    context.fillRect(canvas.width / 2, canvas.height / 2, 50, 50);
-    context.stroke();
+var x = 0;
+var y = 0;
+var width = 50;
+var height = 50;
+var move_value = 20;
 
+function draw() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.beginPath();
+  context.arc(x, y, 25, 0, 2 * Math.PI);
+  context.fillStyle = "red";
+  context.stroke();
 }
+
+function shape_move(key_pressed) {
+  key_value = key_pressed.key;
+  console.log(key_pressed.key);
+  if (key_value === "e") {
+    y -= move_value;
+  } else if (key_value === "s") {
+    x -= move_value;
+  } else if (key_value === "d") {
+    y += move_value;
+  } else if (key_value === "f") {
+    x += move_value;
+  } else if (key_value === "w") {
+    x -= move_value;
+    y -= move_value;
+  } else if (key_value === "a") {
+    x -= move_value;
+    y += move_value;
+  } else if (key_value === "g") {
+    x += move_value;
+    y += move_value;
+  } else if (key_value === "r") {
+    x += move_value;
+    y -= move_value;
+  }
+  draw();
+}
+
+window.addEventListener("keypress", shape_move);
 
 window.requestAnimationFrame(draw);
