b8af82592067c134eb9f3294eff52de4ce897510
NicholasRevenco <nicholas.revenco.25@bishops.com>
2022-12-21 08:29:12 -0800

    Have one apply_velocity function

--- main_function.js
+++ main_function.js
@@ -21,8 +21,11 @@ var circle_velocity = [0, 0, 0];
 var eliminate_position = [700, 700, 15];
 var eliminate_velocity = [Math.round(Math.random()) ? 1 : -1 * 2, Math.round(Math.random()) ? 1 : -1 * 2, 0, 2, 0];
 
+var play_position = [0, 700, 15];
+var play_velocity = [Math.round(Math.random()) ? 1 : -1 * 2, Math.round(Math.random()) ? 1 : -1 * 2, 0, 2, 0];
+
 function eliminate_stop(eliminate_x, eliminate_y, player_x, player_y) {
-    if ((Math.abs(eliminate_x - player_x) < 40) && (Math.abs(eliminate_y - player_y) < 40)) {
+    if ((Math.abs(eliminate_x - player_x) < 35) && (Math.abs(eliminate_y - player_y) < 35)) {
         return true;
     } else {
         return false;
@@ -31,40 +34,19 @@ function eliminate_stop(eliminate_x, eliminate_y, player_x, player_y) {
 
 // Change the position of the circle
 function apply_velocity(which_position, which_velocity, position, velocity) {
-    if (circle_position[0] < 30) {
-        circle_velocity[0] = 2;
-        circle_position[0] = 30;
-    } else if (circle_position[0] > canvas.width - 30) {
-        circle_velocity[0] = -2;
-        circle_position[0] = canvas.width - 30;
+    if (which_position[0] < 30) {
+        which_velocity[0] = 2;
+        which_position[0] = 30;
+    } else if (which_position[0] > canvas.width - 30) {
+        which_velocity[0] = -2;
+        which_position[0] = canvas.width - 30;
     }
-    if (circle_position[1] < 30) {
-        circle_velocity[1] = 2;
-        circle_position[1] = 30;
-    } else if (circle_position[1] > canvas.height - 30) {
-        circle_velocity[1] = -2;
-        circle_position[1] = canvas.height - 30;
-    }
-    var i = 0;
-    for (i = 0; i < position.length; i++) {
-        position[i] += velocity[i];
-    }
-}
-
-function apply_velocity_eliminate(position, velocity) {
-    if (eliminate_position[0] < 30) {
-        eliminate_velocity[0] = 2;
-        eliminate_position[0] = 30;
-    } else if (eliminate_position[0] > canvas.width - 30) {
-        eliminate_velocity[0] = -2;
-        eliminate_position[0] = canvas.width - 30;
-    }
-    if (eliminate_position[1] < 30) {
-        eliminate_velocity[1] = 2;
-        eliminate_position[1] = 30;
-    } else if (eliminate_position[1] > canvas.height - 30) {
-        eliminate_velocity[1] = -2;
-        eliminate_position[1] = canvas.height - 30;
+    if (which_position[1] < 30) {
+        which_velocity[1] = 2;
+        which_position[1] = 30;
+    } else if (which_position[1] > canvas.height - 30) {
+        which_velocity[1] = -2;
+        which_position[1] = canvas.height - 30;
     }
     var i = 0;
     for (i = 0; i < position.length; i++) {
@@ -96,7 +78,8 @@ class create_eliminate {
 // Draw the circle
 function draw() {
     apply_velocity(circle_position, circle_velocity, circle_position, circle_velocity);
-    apply_velocity_eliminate(eliminate_position, eliminate_velocity);
+    apply_velocity(eliminate_position, eliminate_velocity, eliminate_position, eliminate_velocity);
+    apply_velocity(play_position, play_velocity, play_position, play_velocity);
 
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.beginPath();
@@ -107,6 +90,12 @@ function draw() {
     context.beginPath();
     context.fillStyle = "red";
     context.arc(eliminate_position[0], eliminate_position[1], eliminate_position[2], 0, 2 * Math.PI);
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.fillStyle = "blue";
+    context.arc(play_position[0], play_position[1], play_position[2], 0, 2 * Math.PI);
     context.fill();
     context.stroke();
 
